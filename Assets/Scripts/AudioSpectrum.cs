ff7c337c97289f97fa80a441429c515014492d70
Marcos Gabriel <marcosgabrielgx@gmail.com>
2020-01-05 19:47:12 -0300

    Criado Scripts AudioSpectrum e InstantiateCubes, track adicionada, prefabs do Cube criado, Materiais criados

---
+++ Assets/Scripts/AudioSpectrum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class AudioSpectrum : MonoBehaviour
+{
+    public static float[] _sample = new float[512];
+    AudioSource _audioSource;
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_sample,0,FFTWindow.Blackman);
+    }
+}
---
+++ Assets/Scripts/InstatiateCubes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstatiateCubes : MonoBehaviour
+{
+    public GameObject sampleCubesPrefab;
+    GameObject[] _SampleCube = new GameObject[512];
+    public float maxScale;
+    void Start()
+    {
+       for (int i = 0; i< 512; i++)
+        {
+            GameObject instantiateCube = (GameObject)Instantiate(sampleCubesPrefab);
+            instantiateCube.transform.position = this.transform.position;
+            instantiateCube.transform.parent = this.transform;
+            instantiateCube.name = "SampleCube" + i;
+            this.transform.localEulerAngles = new Vector3(0, -0.703125f * i, 0);
+            instantiateCube.transform.position = Vector3.forward * 100;
+            _SampleCube[i] = instantiateCube;
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i<512; i++)
+        {
+            if(_SampleCube != null)
+            {
+                _SampleCube[i].transform.localScale = new Vector3(10, (AudioSpectrum._sample[i] * maxScale)  + 2,10);
+            }
+        }
+    }
+}
